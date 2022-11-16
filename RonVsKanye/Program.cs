e933077df51f5fdd1b7629ad306d1fddd55fb363
Jacob Phillips <jakephillips.wv@gmail.com>
2022-11-15 20:14:16 -0600

    initial commit

---
+++ RonVsKanye/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq; 
+using System.Text.Json.Nodes;
+using static RonVsKanye.RonVSKayneAPI;
+
+var client = new HttpClient();
+var ronVSKanye = new RonVSKanyeAPI(client);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Kanye: \"{ronVSKanye.KayneQuote()}.\"");
+    Console.WriteLine();
+    Thread.Sleep(500);
+    Console.WriteLine($"Ron: {ronVSKanye.RonQuote()}");
+    Console.WriteLine();
+    Thread.Sleep(500);
+}
